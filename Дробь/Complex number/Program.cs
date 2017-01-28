960da885619f711a2fda4c7e2fe34df86895d8d8
abylkasymovkuat <abylkasymov.kuat@gmail.com>
2017-01-28 19:06:26 +0600

    Added comments

--- Дробь/Complex number/Program.cs
+++ Дробь/Complex number/Program.cs
@@ -8,12 +8,12 @@ namespace Complex
 {
     class Program
     {
-        class Complex //create a class
+        class Complex //create a class 
         {
-            //added datas
+            //added data
             public int x;
             public int y;
-            public Complex(int x, int y) //create a constructor
+            public Complex(int x, int y) //make a constructor
             {
                 //initialize class attributes
                 this.x = x;
@@ -66,77 +66,78 @@ namespace Complex
 
                 return c;
             }
-            static void Main(string[] args)
+            static void Main(string[] args) // function to split complex numbers
             {
                 string s = Console.ReadLine();
-                string[] arr = s.Split();
-                Complex sum = new Complex(0, 0);
-                foreach (string ss in arr)
+                string[] arr = s.Split(); // breaks a complex numbers into parts until space: 1/2 3/4
+                Complex sum = new Complex(0, 0); // created class and declared atributes as zeros
+                foreach (string ss in arr) // instead of for we use foreach to work with string array
                 {
-                    string[] t = ss.Split('/');
+                    string[] t = ss.Split('/'); // breaks a complex numbers into parts until "/": 1 2 3  4
                     Complex p = new Complex(int.Parse(t[0]), int.Parse(t[1]));
-                    if (sum.x == 0 & sum.y == 0)
+                    if (sum.x == 0 & sum.y == 0) // because we have declared class as zeros, first condition is always true
                     {
                         sum = p;
                     }
                     else
                     {
-                        sum = sum + p;
+                        sum = sum + p; // adds one complex number to another 
                     }
                 }
 
-                Complex sub = new Complex(0, 0);
+                Complex sub = new Complex(0, 0); // created class and declared atributes as zeros
                 foreach (string ss in arr)
                 {
-                    string[] t = ss.Split('/');
+                    string[] t = ss.Split('/');// breaks a complex numbers into parts until "/": 1 2 3  4
                     Complex p = new Complex(int.Parse(t[0]), int.Parse(t[1]));
-                    if (sub.x == 0 & sub.y == 0)
+                    if (sub.x == 0 & sub.y == 0) // because we have declared class as zeros, first condition is always true
                     {
-                        sub = p;
+                        sub = p; // we declare first element as an first complex number that we write through console
+                                 
                     }
                     else
                     {
                         sub = sub - p;
                     }
                 }
-                Complex mul = new Complex(0, 0);
-                foreach (string ss in arr)
+                Complex mul = new Complex(0, 0); // created class and declared atributes as zeros
+                foreach (string ss in arr) // 
                 {
                     string[] t = ss.Split('/');
                     Complex p = new Complex(int.Parse(t[0]), int.Parse(t[1]));
-                    if (mul.x == 0 & mul.y == 0)
+                    if (mul.x == 0 & mul.y == 0) // because we have declared class as zeros, first condition is always true
                     {
                         mul = p;
+                         // we declare first element as an first complex number that we write through console
+                         // if we wrote like that  mul.x = p.x;  mul.x = p.y;, it would be easier to understand what happens
+
                     }
                     else
                     {
-                        mul = mul * p;
+                        mul = mul * p; // multiply current complex number with previous
                     }
                 }
 
                 Complex div = new Complex(0, 0);
-                foreach (string ss in arr)
+                foreach (string ss in arr) // instead of for we use foreach to work with string array
                 {
                     string[] t = ss.Split('/');
                     Complex p = new Complex(int.Parse(t[0]), int.Parse(t[1]));
                     if (div.x == 0 & div.y == 0)
                     {
-                        div = p;
+                        div = p; // we declare first element as an first complex number that we write through console
                     }
                     else
                     {
-                        div = div / p;
+                        div = div / p; // divides current complex number by previous
                     }
 
                 }
-              
-
-                
-
-                Console.WriteLine(sum);
-                Console.WriteLine(sub);
-                Console.WriteLine(div);
-                Console.WriteLine(mul);
+                // coz we have changed Tostring(), now it shows atributs of complex number as an complex number
+                Console.WriteLine(sum); 
+                Console.WriteLine(sub); 
+                Console.WriteLine(div); 
+                Console.WriteLine(mul); 
 
 
                 Console.ReadKey();
