5ffd6bf350b21237c9557e699e450a18ed6a8891
abylkasymovkuat <abylkasymov.kuat@gmail.com>
2017-01-30 21:11:23 +0600

    Added comments

--- Student Class/Studento/Program.cs
+++ Student Class/Studento/Program.cs
@@ -9,21 +9,22 @@ namespace Studento
     {
         class Student //создаем новый класс
         {
+// обьявляем атрибуты класса
             public string name;
             public string surname;
             public double gpa;
-            public Student(string name, string surname) // конструктор
+            public Student(string name, string surname) // конструктор, в котором отсутствует ГПА
             {
-                this.name = name; // указываем обьект
+                this.name = name; // используем указатель this, с помощью которого указываем текущий обьект
                 this.surname = surname;
             }
-            public Student(string name, double gpa) // конструктор
+            public Student(string name, double gpa) // конструктор, в котором отсутствует фамилия
             {
                 this.name = name;
                 this.gpa = gpa;
             }
 
-            public override string ToString() // функция возврощающая данные в виде string
+            public override string ToString() // Переписали метод Tostrin(). Функция возврощающая данные в виде string
             {
                 return name + ' ' + surname + ' ' + gpa;
             }
@@ -32,11 +33,11 @@ namespace Studento
 
         static void Main(string[] args)
         {
-            Student a = new Student("RustemBek", "Ualikhanov"); // задаем все значения
+            Student a = new Student("RustemBek", "Ualikhanov"); // инициализируем значения класса. При этом конструктор определяется автоматически
             a.gpa = 2.48;
             Student x = new Student("Aidos", 2.36);
             x.surname = "Baibek" ;
-            Console.WriteLine(a);
+            Console.WriteLine(a);  // вывод на экран
             Console.WriteLine(x); // вывод на экран
             Console.ReadKey(); // окно не будет закрыто пока мы не нажмем какую-либо клавишу
         }
